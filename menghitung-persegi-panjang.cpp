6a5f9d71658fa3633ebd8283aae493b231960664
uniquecoded <ferlanferlani.id@gmail.com>
2022-10-01 15:54:01 +0700

    first commit

---
+++ menghitung-persegi-panjang.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int panjang;
+    int lebar;
+    int hasil;
+
+    cout << "------------------------------------" <<endl;
+    cout << "PENGHITUNG PERSEGI PANJANG" << endl;
+    cout << "------------------------------------" <<endl;
+
+    cout << "Masukkan Nilai Panjang : "; cin >> panjang;
+    cout << "Masukkan Nilai Lebar   : "; cin >> lebar;
+
+    hasil = panjang * lebar;
+    
+    cout << "Hasil   : " <<hasil <<endl;
+    cout << "------------------------------------" <<endl;
+
+    cout << "---------------------" <<endl;
+    cout << "Author : Uniquecoded" <<endl;
+    cout << "---------------------" <<endl;
+    
+    cin.get();
+    
+}
